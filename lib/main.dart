62d1b972efb8e3cb306b3769fe678f198d9036db
muhyasin89 <muhyasin89@gmail.com>
2020-04-29 06:59:12 +0700

    [UPDATE]complete getting start with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(GettingStartedApp());
 
-class GettingStartedApp extends StatelessWidget{
+class GettingStartedApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: "Getting Started",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("App bar Title")
-        ),
-        body: new Center(
-          child: new Text("new Center"),
-        )
-      )
+        title: "Getting Started",
+        home: new Scaffold(
+          appBar: new AppBar(title: new Text("App bar Title")),
+          body: new HomeWidget(),
+        ));
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, rowNumber) {
+        return new Container(
+            padding: new EdgeInsets.all(16.0),
+            child: new Column(children: <Widget>[
+              new Image.network("http://shorturl.at/jxBD4"),
+              new Text(
+                "check new Fallout store",
+                style:
+                    new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+              ),
+            ]));
+      },
     );
   }
 }
