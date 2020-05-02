daefea7abc03d899584f2461ca94ccdc3a68ff60
muhyasin89 <muhyasin89@gmail.com>
2020-05-02 14:43:18 +0700

    [UPDATE]real world app

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(GettingStartedApp());
 
-class GettingStartedApp extends StatelessWidget {
+void main() => runApp(new  RealWorldApp());
+
+class RealWorldApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RealWorldState();  
+  }
+}
+
+  
+class RealWorldState  extends State<RealWorldApp>{
+  var _isLoading = true;
+
+  _fetchData() async{
+    print("Attempting to fetch data from networks");
+
+    final url = "http://api.letsbuildthatapp.com/youtube/home_feed";
+    final response = await http.get(url);
+
+    if (response.statusCode == 200){
+      print(response.body);
+    }
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-        title: "Getting Started",
-        home: new Scaffold(
-          appBar: new AppBar(title: new Text("App bar Title")),
-          body: new HomeWidget(),
-        ));
-  }
-}
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Real World App Bar"),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.refresh), 
+            onPressed: (){
+              print("Reloading....");
+              setState(() {
+                _isLoading = false;
+              });
 
-class HomeWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, rowNumber) {
-        return new Container(
-            padding: new EdgeInsets.all(16.0),
-            child: new Column(children: <Widget>[
-              new Image.network("http://shorturl.at/jxBD4"),
-              new Text(
-                "check new Fallout store",
-                style:
-                    new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-              ),
-            ]));
-      },
+              _fetchData();
+            })
+          ],
+        ),
+        body: new Center(
+          child: _isLoading ?  new CircularProgressIndicator():
+          new Text("Loading is Finished...."),
+        ),
+      ),
     );
   }
 }
