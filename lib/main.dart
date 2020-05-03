02add20149950b16d7e17b878689e70d9e0bc94d
muhyasin89 <muhyasin89@gmail.com>
2020-05-03 08:37:22 +0700

    [UPDATE]change list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+void main() => runApp(new RealWorldApp());
 
-void main() => runApp(new  RealWorldApp());
-
-class RealWorldApp extends StatefulWidget{
+class RealWorldApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return new RealWorldState();  
+    return new RealWorldState();
   }
 }
 
-  
-class RealWorldState  extends State<RealWorldApp>{
+class RealWorldState extends State<RealWorldApp> {
   var _isLoading = true;
+  var videos;
 
-  _fetchData() async{
+  _fetchData() async {
     print("Attempting to fetch data from networks");
 
     final url = "http://api.letsbuildthatapp.com/youtube/home_feed";
     final response = await http.get(url);
 
-    if (response.statusCode == 200){
+    if (response.statusCode == 200) {
       print(response.body);
+
+      final map = jsonDecode(response.body);
+      final videosJson = map["videos"];
+
+      // videosJson.forEach((video){
+      //   print(map["videos"]);
+      // });
+
+      setState(() {
+        _isLoading = false;
+        this.videos = videosJson;
+      });
     }
-    
   }
 
   @override
@@ -36,20 +47,44 @@ class RealWorldState  extends State<RealWorldApp>{
         appBar: new AppBar(
           title: new Text("Real World App Bar"),
           actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.refresh), 
-            onPressed: (){
-              print("Reloading....");
-              setState(() {
-                _isLoading = false;
-              });
+            new IconButton(
+                icon: new Icon(Icons.refresh),
+                onPressed: () {
+                  print("Reloading....");
+                  setState(() {
+                    _isLoading = false;
+                  });
 
-              _fetchData();
-            })
+                  _fetchData();
+                })
           ],
         ),
         body: new Center(
-          child: _isLoading ?  new CircularProgressIndicator():
-          new Text("Loading is Finished...."),
+          child: _isLoading
+              ? new CircularProgressIndicator()
+              : new ListView.builder(
+                  itemCount: this.videos != null ? this.videos.length : 0,
+                  itemBuilder: (context, i) {
+                    final video = this.videos[i];
+                    return new Column(
+                      children: <Widget>[
+                        new Container(
+                          padding: new EdgeInsets.all(16.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Image.network(video["imageUrl"]),
+                              new Container(height: 8.0,),
+                              new Text(video["name"],
+                              style: new TextStyle(fontSize: 16.0,
+                              fontWeight: FontWeight.bold),),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
         ),
       ),
     );
